7706d0b0d53dd0efe8a601cf50af804ad8f4a45a
Almira22 <almira199625@gmail.com>
2023-07-23 16:31:47 -0500

    first

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
+require ("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose =require ("mongoose");
 const date = require(__dirname + "/date.js");
 const LoDashStatic = require("lodash");
+const PORT = process.env.PORT || 3000;
 
 
 const app = express();
@@ -149,8 +151,8 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(PORT, function() {
+  console.log(`Server started on port ${PORT}`);
 });
 
 
